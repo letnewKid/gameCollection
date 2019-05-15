319437d227bbccab578d4fa3cf74aaac08baa2eb
letnewKid <tevinmcquilkin@gmail.com>
2019-05-15 14:08:48 -0400

    tm - added raw base seed date for db

--- db/Seed/databaseSeed.js
+++ db/Seed/databaseSeed.js
@@ -1,12 +1,18 @@
 const GameEntry = require('../models/gameEntry');
+const rawData = require('../Seed/rawSeedData');
 
-const fakeGame = {
-  name: 'tevin',
-  condition: 'badbad',
-  Qty: 100,
-};
-const newEntry = new GameEntry(fakeGame);
-console.log('I happened');
+const seedData = rawData.reduce((acc, currEntry) => {
+  const { A, B, C, D } = currEntry;
+  const newEntry = {
+    name: A,
+    console: 'gamecube',
+    condition: B,
+    Qty: C,
+    comment: D,
+  };
+  acc.push(newEntry);
+  return acc;
+}, []);
 
-newEntry.save();
-module.exports = newEntry;
+const seeded = GameEntry.insertMany(seedData);
+module.exports = seeded;
