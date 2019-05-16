d38b95fa1c1302b7d64772d52e1cadfb1d5b7855
letnewKid <tevinmcquilkin@gmail.com>
2019-05-15 21:38:00 -0400

    Added userController functionality to mongodb

--- db/controllers/userController.js
+++ db/controllers/userController.js
@@ -2,11 +2,26 @@ const User = require('../models/userModel');
 
 const userController = {
   addUser: (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+    const newUser = new User({ username, password });
+    newUser.save({ username, password });
+    return res.json({ user: username });
+
     // in the req body I want to be able to access the user name and pass work
     // before adding the user to the database make sure that the user has both a username and password
     // added user to the database
   },
-  loginUser: (req, res) => {
+  verifyUser: (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({ username, password }, err => {
+      if (err)
+        return res
+          .status(404)
+          .json({ user: username, verifyiedCondition: false });
+
+      return res.json({ user: username, verifyiedCondition: true });
+    });
     // checks if the username name and password fields are there
     // throw and error if it not field
   },
--- db/models/userModel.js
+++ db/models/userModel.js
@@ -6,4 +6,4 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
 });
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('users', UserSchema);
